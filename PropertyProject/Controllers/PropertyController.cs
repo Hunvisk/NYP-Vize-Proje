177ba0ac318ec2a6dc6fc45be5e42bc6d1541f9d
Hunvisk <emirhan1ceylan@gmail.com>
2024-04-05 01:46:38 +0300

    Add project files.

---
+++ PropertyProject/Controllers/PropertyController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PropertyProject.Controllers
+{
+    public class PropertyController : Controller
+    {
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+        public IActionResult ForSale()
+        {
+            return View();
+        }
+        public IActionResult ForRent()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult PropertyNews()
+        {
+            return View();
+        }
+    }
+}
---
+++ PropertyProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PropertyProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
